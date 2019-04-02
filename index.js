27add7470ecbfdc5ae55e2e066c3f0b2deee7acd
Lester Dela Cruz <lgdelacruz@Lesters-MacBook-Pro.local>
2019-04-02 09:25:23 -0700

    Added ability to see user sign in status in gmail

--- index.js
+++ index.js
@@ -4,11 +4,14 @@
   gmail integration application
 
   Table of Contents:
-  1. Gmail API: Gmail API related code
-  2. Functions
+  1. Gmail Listener: Listens for changes in Gmail and updates the
+  dom
+  2. Gmail API: Gmail API related code
+  3. Functions
     1. initListeners: Initializes listeners for the app
+    2. untoggleAllMenuItems: Simply untoggles the menu items
   
-  3. Classes
+  4. Classes
     1. Observable: Based on Observer Pattern. This notifies 
     all observers.
     2. Observer: Based on Observer Pattern. This is the templated
@@ -20,8 +23,61 @@
 */
 window.onload = () => {
   initListeners();
+  initGmailListeners();
   loadGmailClient();
 };
+
+/*
+  Gmail Listener
+*/
+
+let gmailObservable;
+
+const initGmailListeners = () => {
+  gmailObservable = new GmailObservable();
+  let gmailSigninObserver = new GmailSigninObserver();
+  gmailObservable.addObserver(gmailSigninObserver);
+};
+
+class GmailObservable {
+  constructor() {
+    this.observers = [];
+    this.addObserver = this.addObserver.bind(this);
+    this.notifyObservers = this.notifyObservers.bind(this);
+  }
+
+  addObserver(o) {
+    this.observers.push(o);
+  }
+
+  notifyObservers(event) {
+    this.observers.forEach(observer => {
+      observer.update(event);
+    });
+  }
+}
+
+class GmailObserver {
+  constructor() {
+    this.update = this.update.bind(this);
+  }
+
+  update() {}
+}
+
+class GmailSigninObserver extends GmailObserver {
+  constructor() {
+    super();
+  }
+
+  update(event) {
+    if (event.signedIn) {
+      console.log("User is signed in!");
+    } else {
+      console.log("User is logged out!");
+    }
+  }
+}
 
 /*
   Gmail API
@@ -55,11 +111,7 @@ const loadGmailClient = () => {
 };
 
 const updateSigninStatus = isSignedIn => {
-  if (isSignedIn) {
-    console.log("User is signed in");
-  } else {
-    console.log("User is logged out");
-  }
+  gmailObservable.notifyObservers({ signedIn: isSignedIn });
 };
 
 const gmailSignIn = () => {
@@ -86,6 +138,13 @@ const initListeners = () => {
   });
 };
 
+const untoggleAllMenuItems = () => {
+  let navButtons = document.querySelectorAll(".nav-menu-item");
+  navButtons.forEach(navButton => {
+    navButton.classList.remove("nav-toggled");
+  });
+};
+
 /*
   Classes
  */
@@ -101,12 +160,9 @@ class Observable {
     this.observers.push(o);
   }
 
-  notifyObservers(message) {
+  notifyObservers(event) {
     this.observers.forEach(observer => {
-      observer.update({
-        event: message.event,
-        gmailStatus: message.gmailStatus
-      });
+      observer.update(event);
     });
   }
 }
@@ -118,13 +174,6 @@ class Observer {
 
   update() {}
 }
-
-const untoggleAllMenuItems = () => {
-  let navButtons = document.querySelectorAll(".nav-menu-item");
-  navButtons.forEach(navButton => {
-    navButton.classList.remove("nav-toggled");
-  });
-};
 
 class NavToggleObserver extends Observer {
   constructor() {
